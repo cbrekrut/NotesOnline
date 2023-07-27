f4d7d6a8b5a6707573b33f27861a67133e12ebd3
Alex <aleksandreremeev75@gmail.com>
2023-07-27 21:54:40 +0300

    Add Style.css file

--- notesonline/main/views.py
+++ notesonline/main/views.py
@@ -3,11 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h1>TestNet</h4>")
-
-def about(request):
-    return HttpResponse("<h1>TestAbout</h4>")
-
-def html(request):
-    name = input()  # Пример динамических данных
-    return render(request, 'index.html', {'name': name})
+    return render(request,'index.html')
